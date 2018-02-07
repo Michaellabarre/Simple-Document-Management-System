f25dd9e89cd7747f82f31de2a76ff1db18ac679d
raf <rafalmo18@gmail.com>
2018-02-07 15:45:58 +0800

    Action Model

--- app/Action.php
+++ app/Action.php
@@ -1,10 +1,12 @@
 <?php
 namespace App;
 use Illuminate\Database\Eloquent\Model;
+
 class Action extends Model
 {
     protected $guarded = [];
     protected $table = 'tbl_act_document';
+
     public function document()
     {
         return $this->belongsTo(Document::class);
